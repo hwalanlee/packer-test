fee6fb84aada1c93a0ebc85f17b87a9f6b13fdfe
hwalanlee <hwalan.lee@bespinglobal.com>
2021-11-30 17:24:41 +0900

    e

--- project3-with-script-file/setup.sh
+++ project3-with-script-file/setup.sh
@@ -1,10 +1,15 @@
 sleep 30
 sudo yum update
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
-# sudo yum install git
-# git clone https://github.com/hwalanlee/simple-nodejs.git
-# cd simple-nodejs/
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+# . ~/.nvm/nvm.sh
+# nvm install node
+sudo amazon-linux-extras install epel -y
+sudo yum-config-manager --enable epel
+sudo yum -y install nodejs
+sudo yum install git
+git clone https://github.com/hwalanlee/simple-nodejs.git
+cd simple-nodejs/
 sudo npm install forever -g
-# sudo forever start index.js
+sudo forever start index.js
+
+
