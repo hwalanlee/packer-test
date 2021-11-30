9e0b0d10b6c63f5fb4846f3f4493ecdac7432f57
hwalanlee <hwalan.lee@bespinglobal.com>
2021-11-30 18:19:14 +0900

    e

--- project4-with-file-provision/setup.sh
+++ project4-with-file-provision/setup.sh
@@ -7,8 +7,9 @@ sudo yum -y install git
 git clone https://github.com/hwalanlee/simple-nodejs.git
 cd simple-nodejs/
 sudo npm install forever -g
-sudo crontab -l > crontab_new 
-sudo echo "@reboot /home/ec2-user/atbooting.sh" >> crontab_new
-sudo crontab crontab_new
-sudo rm crontab_new
+# sudo crontab -l > crontab_new 
+# sudo echo "@reboot /home/ec2-user/atbooting.sh" >> crontab_new
+sudo echo "@reboot /home/ec2-user/atbooting.sh" >> crontab
+# sudo crontab crontab_new
+# sudo rm crontab_new
 sudo chmod +x /home/ec2-user/atbooting.sh
