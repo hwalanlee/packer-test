2647fc04642e1f4d7e1c78ec43a9c080c86b0616
hwalanlee <hwalan.lee@bespinglobal.com>
2021-11-30 16:48:20 +0900

    e

--- project3-with-script-file/setup.sh
+++ project3-with-script-file/setup.sh
@@ -3,7 +3,7 @@ sudo yum update
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
-npm install forever -g
+sudo npm install forever -g
 sudo yum install git
 git clone https://github.com/hwalanlee/simple-nodejs.git
 cd simple-nodejs/
